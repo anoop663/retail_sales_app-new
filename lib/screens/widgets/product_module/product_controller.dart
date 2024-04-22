312f2ac2952f090f1f07f8221fcbb4314027e90b
anoop663 <anoop663@gmail.com>
2024-04-22 17:10:31 +0530

    Commit

--- lib/screens/widgets/product_module/product_controller.dart
+++ lib/screens/widgets/product_module/product_controller.dart
@@ -4,9 +4,6 @@ import 'package:project_fourth/screens/widgets/product_module/product_model.dart
 import 'package:uuid/uuid.dart';
 
 //Category Controllers are given below
-class ProductController {
-  void getAllCategories(){}
-}
 
 ValueNotifier<List<CategoryModel>> categoryListNotifier = ValueNotifier([]);
 
